a865f8023908b12cd1effff87420bd0183a33142
nahee-park <nahee.developer@gmail.com>
2022-11-25 16:35:04 +0900

    feat: wallet connect

--- src/components/common/ConnectWallet.tsx
+++ src/components/common/ConnectWallet.tsx
@@ -1,118 +1,22 @@
 import styled from '@emotion/styled';
-import {
-  ConnectButton,
-  useAccountModal,
-  useChainModal,
-  useConnectModal,
-} from '@rainbow-me/rainbowkit';
+import { useAccountModal, useConnectModal } from '@rainbow-me/rainbowkit';
 import theme from '@src/styles/theme';
+import { useAccount } from 'wagmi';
 
 function ConnectWallet() {
-  // return (
-  // <ConnectButton.Custom>
-  //   {({
-  //     account,
-  //     chain,
-  //     openAccountModal,
-  //     openChainModal,
-  //     openConnectModal,
-  //     authenticationStatus,
-  //     mounted,
-  //   }) => {
-  //     // Note: If your app doesn't use authentication, you
-  //     // can remove all 'authenticationStatus' checks
-  //     const ready = mounted && authenticationStatus !== 'loading';
-  //     const connected =
-  //       ready &&
-  //       account &&
-  //       chain &&
-  //       (!authenticationStatus || authenticationStatus === 'authenticated');
-
-  //     return (
-  //       <div
-  //         {...(!ready && {
-  //           'aria-hidden': true,
-  //           style: {
-  //             opacity: 0,
-  //             pointerEvents: 'none',
-  //             userSelect: 'none',
-  //           },
-  //         })}
-  //       >
-  //         {(() => {
-  //           if (!connected) {
-  //             return (
-  //               <button onClick={openConnectModal} type="button">
-  //                 Connect Wallet
-  //               </button>
-  //             );
-  //           }
-
-  //           if (chain.unsupported) {
-  //             return (
-  //               <button onClick={openChainModal} type="button">
-  //                 Wrong network
-  //               </button>
-  //             );
-  //           }
-
-  //           return (
-  //             <div style={{ display: 'flex', gap: 12 }}>
-  //               <button
-  //                 onClick={openChainModal}
-  //                 style={{ display: 'flex', alignItems: 'center' }}
-  //                 type="button"
-  //               >
-  //                 {chain.hasIcon && (
-  //                   <div
-  //                     style={{
-  //                       background: chain.iconBackground,
-  //                       width: 12,
-  //                       height: 12,
-  //                       borderRadius: 999,
-  //                       overflow: 'hidden',
-  //                       marginRight: 4,
-  //                     }}
-  //                   >
-  //                     {chain.iconUrl && (
-  //                       <img
-  //                         alt={chain.name ?? 'Chain icon'}
-  //                         src={chain.iconUrl}
-  //                         style={{ width: 12, height: 12 }}
-  //                       />
-  //                     )}
-  //                   </div>
-  //                 )}
-  //                 {chain.name}
-  //               </button>
-
-  //               <button onClick={openAccountModal} type="button">
-  //                 {account.displayName}
-  //                 {account.displayBalance ? ` (${account.displayBalance})` : ''}
-  //               </button>
-  //             </div>
-  //           );
-  //         })()}
-  //       </div>
-  //     );
-  //   }}
-  // </ConnectButton.Custom>
-  // <button onClick={openConnectModal}>Connect</button>
+  const { isConnected, address } = useAccount();
   const { openConnectModal } = useConnectModal();
   const { openAccountModal } = useAccountModal();
-  // const { openChainModal } = useChainModal();
 
   return (
     <>
-      {openConnectModal && (
-        <button onClick={openConnectModal} type="button">
-          Open Connect Modal
-        </button>
-      )}
-
-      {openAccountModal && (
+      {isConnected ? (
         <StAddressBtn onClick={openAccountModal} type="button">
-          Open Account Modal
+          {`${address?.slice(0, 3)}...${address?.slice(-3)}`}
+        </StAddressBtn>
+      ) : (
+        <StAddressBtn onClick={openConnectModal} type="button">
+          Connect
         </StAddressBtn>
       )}
     </>
@@ -122,13 +26,12 @@ function ConnectWallet() {
 
 export default ConnectWallet;
 const StAddressBtn = styled.button`
-  padding: 6px 16px;
-
   width: 92px;
   height: 34px;
 
   background: ${theme.colors.gray200};
   border-radius: 100px;
+  font-weight: 700;
 
   color: ${theme.colors.gray800};
   font-size: 14px;
--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,34 +1,20 @@
 import styled from '@emotion/styled';
 import logoImg from '@src/assets/logo.png';
 import theme from '@src/styles/theme';
-import { connectMetamask } from '@src/utils/connectWallet';
 import React from 'react';
+import { useAccount } from 'wagmi';
+
+import ConnectWallet from './ConnectWallet';
 
 interface StLogoBtnProps {
   src: string;
 }
 
 function Header() {
-  const walletAddress = '0xd...024';
-
-  const handleWalletClick = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    switch (e.currentTarget.id) {
-      case 'metamask':
-        console.log(await connectMetamask());
-        break;
-      default:
-        break;
-    }
-  };
-
-  console.log('logoImg', logoImg);
-
   return (
     <StWrap>
       <StLogoBtn src={logoImg.src} />
-      <StMetamaskBtn onClick={handleWalletClick} type="button" id="metamask">
-        {walletAddress}
-      </StMetamaskBtn>
+      <ConnectWallet />
     </StWrap>
   );
 }
@@ -36,7 +22,6 @@ function Header() {
 export default Header;
 
 const StWrap = styled.div`
-  width: 100vw;
   height: 72px;
 
   display: flex;
@@ -51,17 +36,4 @@ const StLogoBtn = styled.button<StLogoBtnProps>`
   height: 55px;
 
   background: url(${(props) => props.src}) center no-repeat;
-`;
-
-const StMetamaskBtn = styled.button`
-  padding: 6px 16px;
-
-  width: 92px;
-  height: 34px;
-
-  background: ${theme.colors.gray200};
-  border-radius: 100px;
-
-  color: ${theme.colors.gray800};
-  font-size: 14px;
 `;

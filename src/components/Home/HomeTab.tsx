900b1f1e3b9598ea26a0601d1a553cac560613f3
Danna <danna.wa@kakaobrain.com>
2022-11-26 17:23:55 +0900

    fix: fix HomeTab style

--- src/components/Home/HomeTab.tsx
+++ src/components/Home/HomeTab.tsx
@@ -2,11 +2,17 @@ import React from 'react';
 
 function HomeTab() {
   return (
-    <div className="mt-1.5 text-center font-medium text-gray-500 border-b border-gray-200 w-full grid grid-cols-3">
-      <button className="tab tab-bordered text-[16px] pb-[10px]">Explore</button>
-      <button className="tab tab-bordered tab-active text-[16px] pb-[10px]">Purchase</button>
-      <button className="tab tab-bordered text-[16px] pb-[10px]">Sale</button>
-    </div>
+    <>
+      <div className={'flex overflow-x-scroll overflow-y-hidden max-w-[100vw]'}>
+        <div className="mt-1.5 m-auto text-center font-semibold text-gray-500 border-b flex gap-5 px-5">
+          <button className="tab text-[16px] pb-10">Explore</button>
+          <button className="tab tab-bordered tab-active text-[16px] pb-10">Purchases</button>
+          <button className="tab text-[16px] whitespace-nowrap pb-10">My NFTs</button>
+          <button className="tab text-[16px] whitespace-nowrap pb-10">Sales</button>
+        </div>
+        <div className="h-[1px] bg-gray-200" />
+      </div>
+    </>
   );
 }
 

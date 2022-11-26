c24030762bfeaa5434202d1c8c07282f7eede970
Danna <danna.wa@kakaobrain.com>
2022-11-26 18:14:34 +0900

    feat: set active button style

--- src/components/common/HomeTab.tsx
+++ src/components/common/HomeTab.tsx
@@ -1,9 +1,27 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useState } from 'react';
 
 function HomeTab() {
   const router = useRouter();
+  const pathName = router.pathname;
+
+  const focusedClass = 'tab tab-bordered tab-active text-[16px] whitespace-nowrap pb-10';
+  const unfocusedClass = 'tab text-[16px] whitespace-nowrap pb-10';
+
+  console.log('pathname', pathName.slice(1));
+
+  const [focusedTab, setFocusedTab] = useState(pathName.slice(1));
+
+  const setTailWindClass = (id: string) => {
+    if (focusedTab == '' && id == 'explore') return focusedClass;
+
+    if (id == focusedTab) return focusedClass;
+    else return unfocusedClass;
+  };
+
   const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setFocusedTab(e.currentTarget.id);
+
     switch (e.currentTarget.id) {
       case 'explore':
         router.push('/');
@@ -18,37 +36,25 @@ function HomeTab() {
         router.push('/sale');
         break;
       default:
+        router.push('/');
         break;
     }
-    // router.push('/');
   };
 
   return (
     <>
       <div className={'flex overflow-x-scroll overflow-y-hidden max-w-[100vw]'}>
         <div className="mt-1.5 m-auto text-center font-semibold text-gray-500 border-b flex gap-5 px-5">
-          <button id={'explore'} className="tab text-[16px] pb-10" onClick={handleClick}>
+          <button id={'explore'} className={setTailWindClass('explore')} onClick={handleClick}>
             Explore
           </button>
-          <button
-            id={'purchase'}
-            className="tab tab-bordered tab-active text-[16px] pb-10"
-            onClick={handleClick}
-          >
+          <button id={'purchase'} className={setTailWindClass('purchase')} onClick={handleClick}>
             Purchases
           </button>
-          <button
-            id={'my'}
-            className="tab text-[16px] whitespace-nowrap pb-10"
-            onClick={handleClick}
-          >
+          <button id={'my'} className={setTailWindClass('my')} onClick={handleClick}>
             My NFTs
           </button>
-          <button
-            id={'sale'}
-            className="tab text-[16px] whitespace-nowrap pb-10"
-            onClick={handleClick}
-          >
+          <button id={'sale'} className={setTailWindClass('sale')} onClick={handleClick}>
             Sales
           </button>
         </div>

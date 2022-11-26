e7958816c7b515f8021b2806d20634f0fe8c3fec
Danna <danna.wa@kakaobrain.com>
2022-11-26 17:41:39 +0900

    feat: link button action

--- src/components/Home/HomeTab.tsx
+++ src/components/Home/HomeTab.tsx
@@ -1,14 +1,56 @@
+import { useRouter } from 'next/router';
 import React from 'react';
 
 function HomeTab() {
+  const router = useRouter();
+  const handleClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    switch (e.currentTarget.id) {
+      case 'explore':
+        router.push('/');
+        break;
+      case 'purchase':
+        router.push('/');
+        break;
+      case 'my':
+        router.push('/');
+        break;
+      case 'sale':
+        router.push('/');
+        break;
+      default:
+        break;
+    }
+    // router.push('/');
+  };
+
   return (
     <>
       <div className={'flex overflow-x-scroll overflow-y-hidden max-w-[100vw]'}>
         <div className="mt-1.5 m-auto text-center font-semibold text-gray-500 border-b flex gap-5 px-5">
-          <button className="tab text-[16px] pb-10">Explore</button>
-          <button className="tab tab-bordered tab-active text-[16px] pb-10">Purchases</button>
-          <button className="tab text-[16px] whitespace-nowrap pb-10">My NFTs</button>
-          <button className="tab text-[16px] whitespace-nowrap pb-10">Sales</button>
+          <button id={'explore'} className="tab text-[16px] pb-10" onClick={handleClick}>
+            Explore
+          </button>
+          <button
+            id={'purchase'}
+            className="tab tab-bordered tab-active text-[16px] pb-10"
+            onClick={handleClick}
+          >
+            Purchases
+          </button>
+          <button
+            id={'my'}
+            className="tab text-[16px] whitespace-nowrap pb-10"
+            onClick={handleClick}
+          >
+            My NFTs
+          </button>
+          <button
+            id={'sale'}
+            className="tab text-[16px] whitespace-nowrap pb-10"
+            onClick={handleClick}
+          >
+            Sales
+          </button>
         </div>
         <div className="h-[1px] bg-gray-200" />
       </div>

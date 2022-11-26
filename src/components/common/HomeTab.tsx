ea069d0cbb2cd003fb326fe01acafa13a6993523
Danna <danna.wa@kakaobrain.com>
2022-11-26 22:11:00 +0900

    fix: fix home tab underline issue

--- src/components/common/HomeTab.tsx
+++ src/components/common/HomeTab.tsx
@@ -59,8 +59,8 @@ function HomeTab() {
             Sales
           </button>
         </div>
-        <div className="h-[1px] bg-gray-200" />
       </div>
+      <div className="h-[1px] bg-gray-200" />
     </>
   );
 }
